ae28051a2cc659649cf05c1f530572061eca6d75
Wahyudi Satriawan Hamid <wahyudisatriawanh@gmail.com>
2026-02-28 13:48:09 +0700

    Praktikum 2.1

--- database/migrations/2026_02_28_064319_create_m_kategori_table.php
+++ database/migrations/2026_02_28_064319_create_m_kategori_table.php
@@ -9,15 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id('kategori_id');
-            $table->string('kategori_kode', 10)->unique();
-            $table->string('kategori_nama', 100);
-            $table->timestamps();
-        });
-    }
+
 
     /**
      * Reverse the migrations.
