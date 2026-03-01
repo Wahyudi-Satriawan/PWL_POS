b12e2b8db2baba2402bafa8e450b9589900db024
Wahyudi Satriawan Hamid <wahyudisatriawanh@gmail.com>
2026-03-01 09:13:12 +0700

    Praktikum 6

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
