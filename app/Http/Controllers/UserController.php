e1bc058fa65ec701834b7ffec16468929ed15d48
Wahyudi Satriawan Hamid <wahyudisatriawanh@gmail.com>
2026-03-04 08:31:30 +0700

    Praktikum 1 - $fillable:

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\UserModel;
 use Illuminate\Http\Request;
+use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -11,9 +11,12 @@ class UserController extends Controller
     public function index()
     {
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
     
         $user = UserModel::all();
         return view('user', ['data' => $user]);
